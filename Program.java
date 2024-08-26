366b73abd1aa04caf9ec2f4b060c2b0bb06b5e3a
quiccklabs05 <adibiswas053@gmail.com>
2024-08-26 10:07:59 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 //import stuff here
 
-//Your code here
-
+//Your code here 
+public class Program7{
+    public static void main(String [] args){
+        //Old System
+        Scanner myScanner = new Scanner (System.in);
+        int oldstanley_nickels = 1;
+        int klevins = 12 * oldstanley_nickels;
+        int oldschrute_bucks = 20 * klevins;
+        
+        oldstanley_nickels = 9;
+        
+        //New System
+        int newschrute_bucks = 100 * oldstanley_nickels;
+        
+        System.out.println("Enter schrute_bucks: " + oldschrute_bucks);
+        
+        System.out.println("Enter klevins: " + klevins);
+        
+        System.out.println("Enter stanley_nickels: " + oldstanley_nickels);
+        
+        System.out.println("Decimal schrute_bucks = " +newschrute_bucks);
+        
+    }
+}
 //Paste console output below:
 /*
 
