fd4c0ac9b4814d1404873744ae2f1651f794185d
quiccklabs05 <adibiswas053@gmail.com>
2024-09-06 20:03:45 -0700

    Update Program.java

--- Program.java
+++ Program.java
@@ -6,11 +6,13 @@ import java.util.Scanner;
 public class Program7{
     public static void main(String [] args){
         //Old System
+        double oldstanleyNickels = 0.0;
+        
         Scanner myScanner = new Scanner (System.in);
         
         double schruteBucks = 0.0;
         double klevins = 0.0;
-        double stanleyNickels = 0.0;
+        double newstanleyNickels = 0.0;
         double decimalSB = 0.0;
         
         //input
@@ -21,20 +23,29 @@ public class Program7{
         klevins = myScanner.nextDouble();
         
         System.out.println("Enter stanley-nickels: ");
-        stanleyNickels = myScanner.nextDouble();
+        newstanleyNickels = myScanner.nextDouble();
+        
         //Calculations
-        klevins = klevins/20.0;
-        stanleyNickels = stanleyNickels/240;
-        decimalSB = (schruteBucks+klevins+stanleyNickels)+0.005;
+        oldstanleyNickels = klevins/12.0;
+        newstanleyNickels = (240 * oldstanleyNickels)/100;
+        decimalSB = (schruteBucks+klevins+newstanleyNickels)+0.005;
+        
         //Round
         decimalSB = (int)(decimalSB*100);
         decimalSB = (int)(decimalSB/100);
         
-        System.out.print("Decimal schrute-bucks:" + decimalSB);
+        
+        System.out.print("Decimal schrute-bucks =" + " " + "$" + decimalSB);
     }
 }
 //Paste console output below:
 /*
-
+Enter schrute-bucks: 
+7
+Enter klevins: 
+17
+Enter stanley-nickels: 
+9
+Decimal schrute-bucks = $27.0
 
 */
